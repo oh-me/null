f9fae09e5a02838a81cf799db9e2614de06e747c
oh-me <58557633+oh-me@users.noreply.github.com>
2022-08-17 14:28:18 +0400

    test
    test

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,4 @@
 let greeting = document.querySelector('.header__greeting');
-let buttonApplyName = document.getElementById('name_apply');
-let nameInput = document.getElementById('greeting_name');
 
 // Greetings
 const night = 'Good night';
@@ -8,31 +6,6 @@ const morning = 'Good morning';
 const afternoon = 'Good afternoon';
 const evening = 'Good evening';
 
-// Name
-if (localStorage.getItem('name') !== null) {
-	let name = localStorage.getItem('name');
-	let greetingName = `<span class="header__greeting-name">${name}</span>.`;
-	setGreeting(greetingName);
-	nameInput.placeholder = name;
-} else {
-	let name = 'Anon';
-	let greetingName = `<span class="header__greeting-name">${name}</span>.`
-	setGreeting(greetingName);
-	nameInput.placeholder = name;
-}
-
-buttonApplyName.onclick = () => {
-	if (nameInput.value !== '') {
-		let greetingName = `<span class="header__greeting-name">${nameInput.value}</span>.`;
-		setGreeting(greetingName);
-
-		localStorage.setItem('name', nameInput.value);
-
-		nameInput.placeholder = nameInput.value;
-		nameInput.value = '';
-		nameInput.blur();
-	}
-};
 
 // Hours of the greetings
 function setGreeting(greetingName) {
