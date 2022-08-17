f8f5c7ee1e1040a8afb3bec03be8752700233523
oh-me <58557633+oh-me@users.noreply.github.com>
2022-08-17 14:39:39 +0400

    sp

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-let buttonSidebar = document.querySelector('.btn_menu');
-buttonSidebar.onclick = () => {
-    let sidebar = document.querySelector('.sidebar-wrapper');
-    sidebar.classList.toggle('show-sidebar');
-};
